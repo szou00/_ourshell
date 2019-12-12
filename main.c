6346d86ed2c76155fac5a44065ee35b668b11c32
Sharon Zou <szou00@stuy.edu>
2019-12-12 05:21:42 -0500

    worked on redirection n piping

--- main.c
+++ main.c
@@ -20,18 +20,36 @@ int main(int argc, char *argv[]){
       int k = 0;
       int m = 0;
       while(com[m] != 0){
+        // printf("in loop. commands, m: %d com[m]: %s\n", m, com[m]);
         m++;
       }
+      // printf("out loop. commands, m: %d com[m]: %s\n", m, com[m]);
       m--;
       while (k != m){
-        if (strcmp(com[k], ">") == 0){
-          redirectout(com, k);
-        }
-        else{
-          k++;
-        }
+        // printf("commands, k: %d com[k]: %s\n", k, com[k]);
+        // printf("%s\n", com);
+          // printf("%s\n", "testing for special character]");
+          if (strcmp(com[k], ">") == 0) {
+            // printf("%s\n", "OUT!"); //testing
+            redirectout(com, k);
+          }
+          else if (strcmp(com[k], "<") == 0) {
+            // printf("%s\n", "IN!");
+            redirectin(com, k);
+          }
+          else if (strcmp(com[k], "|") == 0) {
+            // printf("%s\n", "pipe!");
+            piping(com, k);
+          // }
+          }
+          else {
+            k++;
+          }
+        // printf("m: %d, k: %d\n", m, k);
+        // printf("%s\n", "still here");
       }
       if (k == m){
+        // printf("%s\n", "equal");
         if (strcmp(com[0], "exit") == 0) {
           exit(0);
         }
